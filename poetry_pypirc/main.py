4f71e2085d1356e2ec3a8e3c175401414501dc7f
Narek Amirbekian <narek.amirbekian@airbnb.com>
2022-03-01 19:13:23 -0800

    Fix issues

--- poetry_pypirc/main.py
+++ poetry_pypirc/main.py
@@ -1,26 +1,35 @@
 from configparser import ConfigParser
 from pathlib import Path
+import sys
 import sh
-import toml
 
 PYPIRC_FILE = Path.home() / ".pypirc"
 
 
 def main():
     parser = ConfigParser()
-    parser.read(PYPIRC_FILE)
+    filename = sys.argv[1] if len(sys.argv) > 1 else PYPIRC_FILE
+    parser.read(filename)
     pypirc = parser._sections
 
-    index_servers = (pypirc.get("distutils") or {}).get('index-servers')
+    index_servers = (pypirc.get("distutils") or {}).get('index-servers') or ""
+    index_servers = index_servers.split()
     if not index_servers:
         return
-    
-    config = pypirc.get(index_servers) or {}
-    if config.get("repository"):
-        sh.poetry.config(f"repositories.{index_servers}", config["repository"])
-    
-    if config.get("username") and config.get("password"):
-        sh.poetry.config(f"http-basic.{index_servers}", config.get("username"), config.get("password"))
+
+    for server in index_servers:
+        print(server)
+        server_config = pypirc.get(server) or {}
+        if server_config.get("repository"):
+            print(f"  Setting repositories.{server}.url")
+            sh.poetry.config(
+                f"repositories.{server}", server_config["repository"])
+
+        if server_config.get("username") and server_config.get("password"):
+            print(f"  Setting repositories.{server} auth")
+            sh.poetry.config(
+                f"http-basic.{index_servers}", server_config.get("username"), server_config.get("password"))
+
 
 if __name__ == '__main__':
     main()
